5f4d40df7cd3049f87988d79beb574634b43b817
Takashi SASAKI <takashi316@gmail.com>
2018-03-05 19:10:09 +0900

    support localStorage in Strip.js

--- Strip.js
+++ Strip.js
@@ -44,7 +44,25 @@ function Strip(stripId, stripTitle) {
         return this; // for method chaining
     }; //initByJson
 
+    this.saveToLocalStorage = function(){
+        const stringified = JSON.stringify(this);
+        winwow.localStorage.setItem("stripId=" + this.stripId, stringified);
+    }
+
+    this.loadFromLocalStorage = function(stripId){
+        if(stripId === undefined) stripId = this.stripId;
+        if(stripId === undefined) "Strip#loadFromLocalStorage: stripId is undefined.";
+        const stringified = window.localStorage.getItem("stripId="+stripId);
+        if(typeof stringified !== "string") throw "Strip#loadFromLocalStorage: stripId=" + stripId + " is not found in localStorage.";
+        const parsed = JSON.parse(stringified);
+        for(var x in parsed) {
+          this[x] = parsed[x];
+        }
+    }
+
     Object.defineProperty(this, "initByJson", { enumerable: false });
+    Object.defineProperty(this, "saveToLocalStorage", { enumerable: false });
+    Object.defineProperty(this, "loadFromLocalStorage", { enumerable: false });
     Object.preventExtensions(this);
     return this;
 }
